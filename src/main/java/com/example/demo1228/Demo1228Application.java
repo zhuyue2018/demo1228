66046aabd9687527ba365d584a162172a6649a61
zhuyue <372826057@qq.com>
2018-12-28 15:10:53 +0800

    commit

---
+++ src/main/java/com/example/demo1228/Demo1228Application.java
@@ -0,0 +1,14 @@
+package com.example.demo1228;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1228Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1228Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/demo1228/Demo1228ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo1228;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo1228ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
